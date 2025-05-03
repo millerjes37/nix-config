546206b7435ec45cae6278e475101dee33631cae
Jackson Miller <93346118+millerjes37@users.noreply.github.com>
2025-05-02 23:20:19 -0400

    Fix NixVim configuration structure to match expected format

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -4,14 +4,15 @@
   programs.nixvim = {
     enable = true;
     
-    # ---------- Global options ----------
-    globals = {
-      mapleader = " ";  # Use space as the leader key
-      maplocalleader = ",";
-    };
+    config = {
+      # ---------- Global options ----------
+      globals = {
+        mapleader = " ";  # Use space as the leader key
+        maplocalleader = ",";
+      };
 
-    # Core editor options
-    options = {
+      # Core editor options
+      options = {
       # Display line numbers
       number = true;  
       relativenumber = true;
@@ -57,140 +58,140 @@
       timeoutlen = 500;
     };
     
-    # Core Keymappings
-    keymaps = [
-      # Quick save
-      { 
-        mode = "n"; 
-        key = "<leader>w"; 
-        action = ":w<CR>"; 
-        options.desc = "Save file"; 
-      }
-      
-      # Better window navigation
-      {
-        mode = "n";
-        key = "<C-h>";
-        action = "<C-w>h";
-        options.desc = "Navigate to left window";
-      }
-      {
-        mode = "n";
-        key = "<C-j>";
-        action = "<C-w>j";
-        options.desc = "Navigate to bottom window";
-      }
-      {
-        mode = "n";
-        key = "<C-k>";
-        action = "<C-w>k";
-        options.desc = "Navigate to top window";
-      }
-      {
-        mode = "n";
-        key = "<C-l>";
-        action = "<C-w>l";
-        options.desc = "Navigate to right window";
-      }
-      
-      # Resize windows
-      {
-        mode = "n";
-        key = "<C-Up>";
-        action = ":resize +2<CR>";
-        options.desc = "Increase window height";
-      }
-      {
-        mode = "n";
-        key = "<C-Down>";
-        action = ":resize -2<CR>";
-        options.desc = "Decrease window height";
-      }
-      {
-        mode = "n";
-        key = "<C-Left>";
-        action = ":vertical resize -2<CR>";
-        options.desc = "Decrease window width";
-      }
-      {
-        mode = "n";
-        key = "<C-Right>";
-        action = ":vertical resize +2<CR>";
-        options.desc = "Increase window width";
-      }
-      
-      # Better indenting
-      {
-        mode = "v";
-        key = "<";
-        action = "<gv";
-        options.desc = "Indent left and keep selection";
-      }
-      {
-        mode = "v";
-        key = ">";
-        action = ">gv";
-        options.desc = "Indent right and keep selection";
-      }
-      
-      # Move highlighted text
-      {
-        mode = "v";
-        key = "J";
-        action = ":m '>+1<CR>gv=gv";
-        options.desc = "Move text down";
-      }
-      {
-        mode = "v";
-        key = "K";
-        action = ":m '<-2<CR>gv=gv";
-        options.desc = "Move text up";
-      }
-      
-      # Keep cursor centered during search and joining
-      {
-        mode = "n";
-        key = "n";
-        action = "nzzzv";
-        options.desc = "Next search result (centered)";
-      }
-      {
-        mode = "n";
-        key = "N";
-        action = "Nzzzv";
-        options.desc = "Previous search result (centered)";
-      }
-      {
-        mode = "n";
-        key = "J";
-        action = "mzJ`z";
-        options.desc = "Join lines (keep cursor position)";
-      }
-      
-      # Clear search highlight
-      {
-        mode = "n";
-        key = "<Esc>";
-        action = ":noh<CR>";
-        options.desc = "Clear search highlight";
-      }
-    ];
+      # Core Keymappings
+      keymaps = [
+        # Quick save
+        { 
+          mode = "n"; 
+          key = "<leader>w"; 
+          action = ":w<CR>"; 
+          options.desc = "Save file"; 
+        }
+        
+        # Better window navigation
+        {
+          mode = "n";
+          key = "<C-h>";
+          action = "<C-w>h";
+          options.desc = "Navigate to left window";
+        }
+        {
+          mode = "n";
+          key = "<C-j>";
+          action = "<C-w>j";
+          options.desc = "Navigate to bottom window";
+        }
+        {
+          mode = "n";
+          key = "<C-k>";
+          action = "<C-w>k";
+          options.desc = "Navigate to top window";
+        }
+        {
+          mode = "n";
+          key = "<C-l>";
+          action = "<C-w>l";
+          options.desc = "Navigate to right window";
+        }
+        
+        # Resize windows
+        {
+          mode = "n";
+          key = "<C-Up>";
+          action = ":resize +2<CR>";
+          options.desc = "Increase window height";
+        }
+        {
+          mode = "n";
+          key = "<C-Down>";
+          action = ":resize -2<CR>";
+          options.desc = "Decrease window height";
+        }
+        {
+          mode = "n";
+          key = "<C-Left>";
+          action = ":vertical resize -2<CR>";
+          options.desc = "Decrease window width";
+        }
+        {
+          mode = "n";
+          key = "<C-Right>";
+          action = ":vertical resize +2<CR>";
+          options.desc = "Increase window width";
+        }
+        
+        # Better indenting
+        {
+          mode = "v";
+          key = "<";
+          action = "<gv";
+          options.desc = "Indent left and keep selection";
+        }
+        {
+          mode = "v";
+          key = ">";
+          action = ">gv";
+          options.desc = "Indent right and keep selection";
+        }
+        
+        # Move highlighted text
+        {
+          mode = "v";
+          key = "J";
+          action = ":m '>+1<CR>gv=gv";
+          options.desc = "Move text down";
+        }
+        {
+          mode = "v";
+          key = "K";
+          action = ":m '<-2<CR>gv=gv";
+          options.desc = "Move text up";
+        }
+        
+        # Keep cursor centered during search and joining
+        {
+          mode = "n";
+          key = "n";
+          action = "nzzzv";
+          options.desc = "Next search result (centered)";
+        }
+        {
+          mode = "n";
+          key = "N";
+          action = "Nzzzv";
+          options.desc = "Previous search result (centered)";
+        }
+        {
+          mode = "n";
+          key = "J";
+          action = "mzJ`z";
+          options.desc = "Join lines (keep cursor position)";
+        }
+        
+        # Clear search highlight
+        {
+          mode = "n";
+          key = "<Esc>";
+          action = ":noh<CR>";
+          options.desc = "Clear search highlight";
+        }
+      ];
 
-    # ---------- Colorscheme ----------
-    colorschemes.tokyonight = {
-      enable = true;
-      style = "storm"; # Options: night, storm, day, moon
-      transparent = false;
-      styles = {
-        comments = { italic = true; };
-        keywords = { italic = true; };
-        functions = { bold = true; };
-        variables = {};
+      # ---------- Colorscheme ----------
+      colorschemes.tokyonight = {
+        enable = true;
+        style = "storm"; # Options: night, storm, day, moon
+        transparent = false;
+        styles = {
+          comments = { italic = true; };
+          keywords = { italic = true; };
+          functions = { bold = true; };
+          variables = {};
+        };
       };
-    };
 
-    # ---------- UI Enhancements ----------
-    plugins = {
+      # ---------- UI Enhancements ----------
+      plugins = {
       # Native LSP setup
       lsp = {
         enable = true;
@@ -451,62 +452,63 @@
       nvim-ufo.enable = true;
     };
     
-    # ---------- Custom vim commands ----------
-    extraConfigLua = ''
-      -- Terminal-specific keymaps
-      function _G.set_terminal_keymaps()
-        local opts = {buffer = 0}
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-      end
-
-      -- Run terminal keymaps when opening terminal
-      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
-
-      -- Additional nvim-tree keymaps
-      vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {silent = true})
-      vim.keymap.set('n', '<leader>o', ':NvimTreeFocus<CR>', {silent = true})
-
-      -- Bufferline keymaps
-      vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>', {silent = true})
-      vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>', {silent = true})
-      vim.keymap.set('n', '<leader>q', ':Bdelete<CR>', {silent = true})
-      
-      -- Neogit keymap
-      vim.keymap.set('n', '<leader>gg', ':Neogit<CR>', {silent = true})
-      
-      -- LSP Format keymap
-      vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format({ async = true }) end, {silent = true})
-      
-      -- Diagnostics keymaps
-      vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {silent = true})
-      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {silent = true})
-      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {silent = true})
-      
-      -- UFO folding setup
-      vim.o.foldcolumn = '1'
-      vim.o.foldlevel = 99
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-      
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-      
-      -- Per-buffer LSP attach handler
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-        callback = function(ev)
-          local client = vim.lsp.get_client_by_id(ev.data.client_id)
-          -- Enable inlay hints for supported languages
-          if client and client.server_capabilities.inlayHintProvider then
-            vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
-          end
+      # ---------- Custom vim commands ----------
+      extraConfigLua = ''
+        -- Terminal-specific keymaps
+        function _G.set_terminal_keymaps()
+          local opts = {buffer = 0}
+          vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+          vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+          vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+          vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+          vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
         end
-      })
-    '';
+
+        -- Run terminal keymaps when opening terminal
+        vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+        -- Additional nvim-tree keymaps
+        vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {silent = true})
+        vim.keymap.set('n', '<leader>o', ':NvimTreeFocus<CR>', {silent = true})
+
+        -- Bufferline keymaps
+        vim.keymap.set('n', '<S-l>', ':BufferLineCycleNext<CR>', {silent = true})
+        vim.keymap.set('n', '<S-h>', ':BufferLineCyclePrev<CR>', {silent = true})
+        vim.keymap.set('n', '<leader>q', ':Bdelete<CR>', {silent = true})
+        
+        -- Neogit keymap
+        vim.keymap.set('n', '<leader>gg', ':Neogit<CR>', {silent = true})
+        
+        -- LSP Format keymap
+        vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format({ async = true }) end, {silent = true})
+        
+        -- Diagnostics keymaps
+        vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, {silent = true})
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {silent = true})
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {silent = true})
+        
+        -- UFO folding setup
+        vim.o.foldcolumn = '1'
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+        
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+        
+        -- Per-buffer LSP attach handler
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+          callback = function(ev)
+            local client = vim.lsp.get_client_by_id(ev.data.client_id)
+            -- Enable inlay hints for supported languages
+            if client and client.server_capabilities.inlayHintProvider then
+              vim.lsp.inlay_hint.enable(true, { bufnr = ev.buf })
+            end
+          end
+        })
+      '';
+    }; # End of config section
   };
 }
