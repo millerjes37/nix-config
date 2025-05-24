4eaee9f0c097af7c65898a20598112bfe951c6a8
Jackson Miller <93346118+millerjes37@users.noreply.github.com>
2025-05-23 22:33:24 -0400

    Update configuration: nixvim.nix

--- modules/common/nixvim.nix
+++ modules/common/nixvim.nix
@@ -380,7 +380,8 @@
           sort_by = "case_sensitive";
           view = { width = 30; side = "left"; }; 
           renderer = {
-            group_empty = true, highlight_git = true;
+            group_empty = true; 
+            highlight_git = true;
             icons = {
               show = { file = true, folder = true, folder_arrow = true, git = true };
               glyphs = {
