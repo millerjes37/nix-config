a7d19f1cfa789724a66dfc51ddbd3a3684f2b7d3
Jackson <jackson@civitas.ltd>
2025-06-18 01:33:46 -0400

    fix: use nixGLIntel directly to avoid builtins.currentTime

--- modules/linux/nixgl.nix
+++ modules/linux/nixgl.nix
@@ -1,19 +1,19 @@
 { pkgs, ... }:
 
 let
-  # Pull all nixGL wrappers (Intel, Mesa, Nvidia, etc.)
-  nixGL = pkgs.nixgl.auto;
+  # Use nixGLIntel directly to avoid auto-detection that uses builtins.currentTime
+  nixGLIntel = pkgs.nixgl.nixGLIntel;
 
   # Replace the plain alacritty binary with a wrapper that injects nixGL.
   myAlacritty = pkgs.writeShellScriptBin "alacritty" ''
     #!/usr/bin/env bash
-    exec ${nixGL}/bin/nixGLIntel ${pkgs.alacritty}/bin/alacritty "$@"
+    exec ${nixGLIntel}/bin/nixGLIntel ${pkgs.alacritty}/bin/alacritty "$@"
   '';
 in
 {
   # Make the wrappers available on the system.
   home.packages = [
-    nixGL
+    nixGLIntel
     myAlacritty
   ];
 } 
